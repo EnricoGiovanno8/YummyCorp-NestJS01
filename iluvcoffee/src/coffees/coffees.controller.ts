a1988746c6885cc03839932337b7c842e3c7b6d1
EnricoGiovanno8 <genrico189@gmail.com>
2021-12-24 13:54:28 +0700

    Folder Generating OpenAPI Specification

--- iluvcoffee/src/coffees/coffees.controller.ts
+++ iluvcoffee/src/coffees/coffees.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { Protocol } from 'src/common/decorators/protocol.decorator';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
@@ -7,6 +7,7 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
 
@@ -14,13 +15,10 @@ export class CoffeesController {
         private readonly coffeesService: CoffeesService
     ) {}
 
+    @ApiForbiddenResponse({ description: 'Forbidden' })
     @Public()
     @Get()
-    async findAll(
-        @Protocol('https') protocol: string,
-        @Query() paginationQuery: PaginationQueryDto
-    ) {
-        console.log(protocol)
+    async findAll(@Query() paginationQuery: PaginationQueryDto) {
         return this.coffeesService.findAll(paginationQuery)
     }
 
--- iluvcoffee/src/coffees/dto/create-coffee.dto.ts
+++ iluvcoffee/src/coffees/dto/create-coffee.dto.ts
@@ -1,14 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class CreateCoffeeDto {
 
+    @ApiProperty({ description: 'The name of a coffee.' })
     @IsString()
     readonly name: string;
 
+    @ApiProperty({ description: 'The brand of a coffee.' })
     @IsString()
     readonly brand: string;
 
-    @IsString({ each: true })
+    @ApiProperty({ example: [] })
     readonly flavors: string[];
 
 }
